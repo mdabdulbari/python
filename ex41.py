4e5b2729b32f3002ac5270eaea07a7257354ca31
Abdul Bari <addubari@gmail.com>
2017-06-10 17:04:14 +0530

    Add 4th exercise

---
+++ ex41.py
@@ -0,0 +1,9 @@
+#practice
+x = 153
+y= 236
+z= 133
+print "x + y + z =", x + y + z,"Nice"
+print "x - y + z =", x - y + z, "Good"
+print "x / y + z =", x / y + z, "Very nice"
+print "x * y *z =", x * y * z, "Awesome"
+print "x * y - z =", x * y -z, "xD"
