d2889075a4b390f9f335c920b0c67ef7b5b7e28f
Abdul Bari <addubari@gmail.com>
2017-06-12 18:39:24 +0530

    Added sample exercise 15 and exercise 16

--- ex15_sample3.py
+++ ex15_sample3.py
@@ -1,10 +1,9 @@
-from sys import argv
-
-script, filename = argv
+print("Type the file name")
+filename = input("> ")
 
 txt = open(filename) #opens the file and assigns the text to txt
 
-print(f"Here's your file {filename}:") prints file name
+print(f"Here's your file {filename}:") #prints file name
 print(txt.read())
 
 print("Type the filename again:")
